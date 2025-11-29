70fb315048284adeecd1b0d2600411f95b76414f
s4ndyp <96690821+s4ndyp@users.noreply.github.com>
2025-11-29 17:59:35 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // --- Configuratie ---
 // Zorg voor een nieuwe versie om de cache te forceren
-const CACHE_NAME = 'superlijst-v5';
+const CACHE_NAME = 'superlijst-v6';
 
 // De bestanden die nodig zijn om de app te laden (inclusief externe bibliotheken)
 const urlsToCache = [
